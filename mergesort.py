c1b19f1f3d8497a69a7e004e388c56a415d45c71
Olesia Korchevaia <o.korchevaia@Olesas-MacBook-Air.local>
2025-06-15 14:07:52 +0200

    Added changes to the functions to follow the clean code rules

--- mergesort.py
+++ mergesort.py
@@ -1,45 +1,45 @@
-def ASSIGNMENT(new_list, i, old_list, j):
+import matplotlib.pyplot as plt
+
+
+def assigment(new_list, i, old_list, j):
     new_list[i] = old_list[j]
 
 
-def mergeSort(list_to_sort_by_merge):
-    if (
-        len(list_to_sort_by_merge) > 1
-        and not len(list_to_sort_by_merge) < 1
-        and len(list_to_sort_by_merge) != 0
-    ):
-        mid = len(list_to_sort_by_merge) // 2
-        left = list_to_sort_by_merge[:mid]
-        right = list_to_sort_by_merge[mid:]
+def mergeSort(arr):
+
+    if len(arr) <= 1:
+        return
+
+    else:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
 
         mergeSort(left)
         mergeSort(right)
 
-        l = 0
-        r = 0
+        left_index = 0
+        right_index = 0
         i = 0
 
-        while l < len(left) and r < len(right):
-            if left[l] <= right[r]:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=left, j=l)
-                l += 1
+        while left_index < len(left) and right_index < len(right):
+            if left[left_index] <= right[right_index]:
+                assigment(new_list=arr, i=i, old_list=left, j=left_index)
+                left_index += 1
             else:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=right, j=r)
-                r += 1
+                assigment(new_list=arr, i=i, old_list=right, j=right_index)
+                right_index += 1
             i += 1
 
-        while l < len(left):
-            list_to_sort_by_merge[i] = left[l]
-            l += 1
+        while left_index < len(left):
+            arr[i] = left[left_index]
+            left_index += 1
             i += 1
 
-        while r < len(right):
-            list_to_sort_by_merge[i] = right[r]
-            r += 1
+        while right_index < len(right):
+            arr[i] = right[right_index]
+            right_index += 1
             i += 1
-
-
-import matplotlib.pyplot as plt
 
 my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 x = range(len(my_list))
