0e517c3fa2d269fb659d62e7208c9ac247c42323
Olesia Korchevaia <o.korchevaia@Olesas-MacBook-Air.local>
2025-06-15 14:25:59 +0200

    Improve plotting: bar chart with labels

--- mergesort.py
+++ mergesort.py
@@ -41,11 +41,22 @@ def mergeSort(list_to_sort_by_merge):
 
 import matplotlib.pyplot as plt
 
+def plot_list(data, title):
+    plt.figure(figsize=(10, 4))
+    x = range(len(data))
+    plt.bar(x, data, color='skyblue')
+    plt.title(title)
+    plt.xlabel("Index")
+    plt.ylabel("Value")
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    plt.tight_layout()
+    plt.show()
+
+
 my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-x = range(len(my_list))
-plt.plot(x, my_list)
-plt.show()
+
+plot_list(my_list, "Before Merge Sort")
+
 mergeSort(my_list)
-x = range(len(my_list))
-plt.plot(x, my_list)
-plt.show()
+
+plot_list(my_list, "After Merge Sort")
